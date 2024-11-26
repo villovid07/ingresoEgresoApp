e69bab60f29c559d8e751a575854147ed4a58d61
villovid07 <villovid07@gmail.com>
2024-11-26 21:20:34 +0000

    Template creado

---
+++ src/app/ingreso-egreso/estadistica/estadistica.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-estadistica',
+  templateUrl: './estadistica.component.html',
+  styles: [
+  ]
+})
+export class EstadisticaComponent {
+
+}
---
+++ src/app/ingreso-egreso/ingreso-egreso.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ingreso-egreso',
+  templateUrl: './ingreso-egreso.component.html',
+  styles: [
+  ]
+})
+export class IngresoEgresoComponent {
+
+}
