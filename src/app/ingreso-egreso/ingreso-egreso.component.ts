7c9d9149953f497280f64476f12fce343b0489a0
villovid07 <villovid07@gmail.com>
2025-03-20 11:14:19 -0500

    Create form for incomes and outcomes

--- src/app/ingreso-egreso/ingreso-egreso.component.ts
+++ src/app/ingreso-egreso/ingreso-egreso.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-ingreso-egreso',
   templateUrl: './ingreso-egreso.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class IngresoEgresoComponent {
+  ingresoForm!: FormGroup;
+  tipo: string = 'ingreso';
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.ingresoForm = this.fb.group({
+      descripcion: ['', Validators.required],
+      monto: ['', Validators.required], //add more constratin to this field
+    });
+  }
+
+  guardar() {
+    if (this.ingresoForm.invalid) {
+      return;
+    }
+    console.log(this.ingresoForm.valid);
+    console.log(this.ingresoForm.value);
+    console.log(this.tipo);
+  }
 }
